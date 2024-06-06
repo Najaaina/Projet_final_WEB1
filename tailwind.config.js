7b7c96d55c18821056d19757b89a00b605ed485d
Liantsoa Najaina <hei.najaina@gmail.com>
2024-06-06 15:47:43 +0300

    feat: finish header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./**/*.js", "./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        fuchsia: "#f33cfc",
+        med_slate_blue: "#925BFC",
+        tropic_indigo: "#A778FC",
+        white: "#FDFDFF",
+        oxford_blue: "#0B132B"
+      }
+    },
   },
   plugins: [],
 }
